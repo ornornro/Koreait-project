361287eec5681ad28c75f7364d9e0ea9582a4f7f
ornornro <roornorn@gmail.com>
2023-01-12 21:59:21 +0900

    관리자페이지 구성.

---
+++ src/main/java/com/korit/library/web/dto/SearchReqDto.java
@@ -0,0 +1,35 @@
+package com.korit.library.web.dto;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+public class SearchReqDto {
+    @ApiModelProperty(value = "검색어", required = false, example = "나는")
+    private String searchValue;
+
+    @ApiModelProperty(value = "카테고리",  required = false, example = "소설")
+    private String category;
+
+    @ApiModelProperty(value = "정렬", required = false, example = "bookName")
+    private String order;
+
+    @NotBlank
+    @ApiModelProperty(value = "조회전체 = N, 조회제한 = Y")
+    private String limit;
+
+    @ApiModelProperty(value = "페이지번호", required = false, example = "1")
+    private int page;
+
+    @ApiModelProperty(value = "게시글개수", required = false, example = "20")
+    private int count;
+
+    @ApiModelProperty(hidden = true)
+    private int index;
+
+    public void setIndex() {
+        index = (page - 1) * count;
+    }
+}
