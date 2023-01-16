ede9a597b563ca9cfc9f549e1587c55f05c08f17
ornornro <roornorn@gmail.com>
2023-01-16 21:51:31 +0900

    사진등록 tempFileName, originFileName 설정해서 등록하기1

--- src/main/java/com/korit/library/web/api/admin/BookApi.java
+++ src/main/java/com/korit/library/web/api/admin/BookApi.java
@@ -5,11 +5,15 @@ import com.korit.library.aop.annotation.ValidAspect;
 import com.korit.library.service.BookService;
 import com.korit.library.web.dto.*;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -55,6 +59,34 @@ public class BookApi {
         bookService.modifyBook(bookReqDto);
         return ResponseEntity
                 .ok()
+                .body(new CMRespDto<>(HttpStatus.OK.value(), "Successfully", true));
+    }
+
+    @ParamsAspect
+    @ValidAspect
+    @PatchMapping("/book/{bookCode}") // put 과 patch 의 차이점: put 은 null 일 경우 그대로 입력 patch 는 null 일 경우 유지된다
+    public ResponseEntity<CMRespDto<?>> maintainModifyBook(@PathVariable String bookCode, @Valid @RequestBody BookReqDto bookReqDto, BindingResult bindingResult) {
+        bookService.maintainModifyBook(bookReqDto);
+        return ResponseEntity
+                .ok()
+                .body(new CMRespDto<>(HttpStatus.OK.value(), "Successfully", true));
+    }
+
+    @ParamsAspect
+    @DeleteMapping("/book/{bookCode}") // delete
+    public ResponseEntity<CMRespDto<?>> removeBook(@PathVariable String bookCode) {
+        bookService.removeBook(bookCode);
+        return ResponseEntity
+                .ok()
+                .body(new CMRespDto<>(HttpStatus.OK.value(), "Successfully", true));
+    }
+
+    @ParamsAspect
+    @PostMapping("/book/{bookCode}/images")
+    public ResponseEntity<CMRespDto<?>> registerBookImg(@PathVariable String bookCode, @RequestPart List<MultipartFile> files) {
+        bookService.registerBookImages(bookCode, files);
+
+        return  ResponseEntity.ok()
                 .body(new CMRespDto<>(HttpStatus.OK.value(), "Successfully", true));
     }
 
